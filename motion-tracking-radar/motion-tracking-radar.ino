72b808d79c0d413bd03e8f8793b02034ca408b98
Anelechukwu Jasper <jasperanels@gmail.com>
2025-08-16 22:06:49 +0100

    Logic to move servo motor

--- motion-tracking-radar/motion-tracking-radar.ino
+++ motion-tracking-radar/motion-tracking-radar.ino
@@ -1,13 +1,29 @@
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+
 const uint8_t led_pin = 8;
+const uint8_t servo_pin = 6;
 const uint8_t buzzer_pin = 9;
 const uint32_t buzzer_tone = 1500;
 const uint32_t action_interval = 1000;
 
+Servo servo;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 uint8_t led_state = LOW;
+uint8_t servo_state = LOW;
+uint32_t lcd_counter = 0;
 uint8_t buzzer_state = LOW;
 uint32_t previous_time = 0;
 
 void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Count:");
+
+  servo.attach(servo_pin);
+
   pinMode(led_pin, OUTPUT);
   pinMode(buzzer_pin, OUTPUT);
 }
@@ -22,6 +38,7 @@ void loop() {
 
     digitalWrite(led_pin, led_state);
 
+
     buzzer_state = buzzer_state == LOW ? HIGH : LOW;
 
     if (buzzer_state == HIGH) {
@@ -29,5 +46,22 @@ void loop() {
     } else {
       noTone(buzzer_pin);
     }
+
+
+    lcd_counter++;
+
+    lcd.setCursor(0, 1);
+
+    lcd.print(lcd_counter);
+
+
+    servo_state = servo_state == LOW ? HIGH : LOW;
+
+    if (servo_state == HIGH) {
+      servo.write(180);
+    } else {
+      servo.write(0);
+    }
+
   }
 }
